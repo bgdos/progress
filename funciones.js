8e13b7abe98d317092597002eaf95e9546ccdb71
John Salgado <juansutt@hotmail.com>
2015-05-07 14:01:47 -0700

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function progress()
+{
+    var values = [25, 50, 75, 90, 68, 45, 22];
+    var i = 0;
+    $('#progress:first-child').each(function() { 
+		$(this).find('div').addClass ="v"+i;
+        progressBar(values[i], $(this));
+        i++;
+    });
+}
